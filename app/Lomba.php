7c7ccc93f42956480920df7465341888d44ccb9c
Erick Christian <erickchristian48@gmail.com>
2020-03-12 09:54:33 +0700

    Hei udah berapa kali commit nih?

--- app/Lomba.php
+++ app/Lomba.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lomba extends Model
 {
-    //
+    /**
+     * Mengambil juara 1 untuk lomba ini.
+     */
+    public function juara_1()
+    {
+        return $this->hasOne('Peserta', 'juara_1');
+    }
+
+    /**
+     * Mengambil juara 1 untuk lomba ini.
+     */
+    public function juara_2()
+    {
+        return $this->hasOne('Peserta', 'juara_2');
+    }
+
+    /**
+     * Mengambil juara 1 untuk lomba ini.
+     */
+    public function juara_3()
+    {
+        return $this->hasOne('Peserta', 'juara_3');
+    }
 }
